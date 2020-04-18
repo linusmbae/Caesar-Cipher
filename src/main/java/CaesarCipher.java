7476750fb236d3ea4e952f0d0f826c6de2bcb953
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-18 23:11:53 +0300

    encryption code added

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,9 +1,22 @@
 public class CaesarCipher {
     private String encrypt;
     private int jump;
-
-    public CaesarCipher(String encryptMessage, int jump) {
-
+    StringBuilder charResult=new StringBuilder();
+    public CaesarCipher(String encryptMessage, int key) {
+    for (char character:encryptMessage.toCharArray())
+    {
+        if (character!=' ')
+        {
+            int characterPosition=character - 'a';
+            int newPosition=(characterPosition+key)%26;
+            char newChar=(char)('a'+newPosition);
+            charResult.append(newChar);
+        }else
+        {
+            charResult.append(character);
+        }
+    }
+//    return charResult;
     }
     public String encryptionIsEqual() {
         return "irgvctx";
