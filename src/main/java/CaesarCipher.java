47e899e5725de61cb52a01581be0ed1c296168c3
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-18 23:23:15 +0300

    decryption code added

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -5,10 +5,23 @@ public class CaesarCipher {
     public CaesarCipher(String encryptMessage, int key) {
     for (char character:encryptMessage.toCharArray())
     {
+//        encryption Code
         if (character!=' ')
         {
             int characterPosition=character - 'a';
             int newPosition=(characterPosition+key)%26;
+            char newChar=(char)('a'+newPosition);
+            charResult.append(newChar);
+        }else
+        {
+            charResult.append(character);
+        }
+
+//        decryption code
+        if (character!=' ')
+        {
+            int characterPosition=character - 'a';
+            int newPosition=(characterPosition-key)%26;
             char newChar=(char)('a'+newPosition);
             charResult.append(newChar);
         }else
