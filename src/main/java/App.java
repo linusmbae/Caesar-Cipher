fa8bb1e9ca6f270dd4db09de95ca451d88ea85b6
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-18 23:47:36 +0300

    selection complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,14 @@
+import java.io.Console;
 public class App {
+    public static void main(String[] args)
+    {
+        String [] select={"Encrypt","Decrypt","Exit"};
+        Console console= System.console();
+        System.out.println("Welcome to CaesarCipher App");
+        System.out.println("Actions");
+        for (String selected:select)
+        {
+            System.out.println(selected);
+        }
+    }
 }
